0568dac4ec5be1e75f831d76ffd1bfb2ca8b1223
Lucas A. Brisolla <lucasabrisolla@gmail.com>
2024-03-09 13:19:33 -0300

    Primitive types

---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        int age = 10;
+        long longNumber = 10000;
+        short ageShort = 100;
+        double salaryDouble = 2000.50;
+        float salaryFloat = 2500;
+        byte byteNumber = 127;
+        boolean yes = true;
+        boolean no = false;
+        char character = 'A';
+
+        System.out.println("The age is "+age+" years old");
+        System.out.println(longNumber);
+        System.out.println(ageShort);
+        System.out.println(salaryDouble);
+        System.out.println(salaryFloat);
+        System.out.println(byteNumber);
+        System.out.println(yes);
+        System.out.println(no);
+        System.out.println(character);
+    }
+}
