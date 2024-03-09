a2abf732da0b69c791ebc06441d6891d82632e64
Lucas A. Brisolla <lucasabrisolla@gmail.com>
2024-03-09 16:26:53 -0300

    Conditional structures pt 01

---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main(String[] args) {
+        if(true){
+            System.out.println("Inside the if");
+        }
+        System.out.println("Outside the if");
+    }
+}
