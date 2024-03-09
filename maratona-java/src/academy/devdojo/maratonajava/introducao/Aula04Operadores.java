40a2f922ac272f3936aec3ef00255bb8374ac4d1
Lucas A. Brisolla <lucasabrisolla@gmail.com>
2024-03-09 16:20:05 -0300

    Operators

--- maratona-java/src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -10,13 +10,30 @@ public class Aula04Operadores {
         int mlc = num * num1;
         int dvc = num1 / num;
 
-        // logical operators < > <= => == != booleans AND OR
+        // logical operators < > <= >= == != booleans AND -> && OR -> || not -> !
         boolean isFiveBiggerThanTwo = 5 > 2;
+        int age = 30;
+        float salary = 3500f;
+        boolean isLegalBiggerThanThirty = age >= 30 && salary >= 4612;
+        boolean isLegalSmallerThanThirty = age < 30 && salary >= 3381;
+
+        int bonus = 2800;
+        bonus += 1000;
+        bonus -= 1000;
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+        int counter = 0;
+        counter++;
+        System.out.println(counter);
+        System.out.println(bonus);
 
         System.out.println(adc);
         System.out.println(sbc);
         System.out.println(mlc);
         System.out.println(dvc);
         System.out.println(isFiveBiggerThanTwo);
+        System.out.println(isLegalBiggerThanThirty);
+        System.out.println(isLegalSmallerThanThirty);
     }
 }
