229e49d623c2b597929f021e5d923029b7bf62b7
Lucas A. Brisolla <lucasabrisolla@gmail.com>
2024-03-09 13:14:01 -0300

    Java Repository, Course, my first program, hello world, and intelij setup

---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula1HelloWorld.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula1HelloWorld {
+    // Comentario 1 linha
+    /*
+    Comentario multiplas linhas
+    * */
+    /** Comentario renderizavel <b>javadoc<b>*/
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
