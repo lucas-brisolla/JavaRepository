2ba7c03c3d02fc1cb4b451a1a184cd11e20816d6
Lucas A. Brisolla <lucasabrisolla@gmail.com>
2024-03-09 22:42:29 -0300

    For Exercise Solved

--- maratona-java/src/academy/devdojo/maratonajava/introducao/ForExercise.java
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/ForExercise.java
@@ -1,12 +1,11 @@
 package academy.devdojo.maratonajava.introducao;
 
-/**
- * contar todos os numeros pares de 1 ou de 0 até 1 milhão 1.000.000
- */
 public class ForExercise {
     public static void main(String[] args) {
-        for (int i = 0; i <= 1000000; i += 2){
-            System.out.println(i);
+        for (int i = 0; i <= 1000000; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
         }
     }
 }
