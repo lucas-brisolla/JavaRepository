32ecedbf06314200be0a1f0367c108e9864848af
Lucas A. Brisolla <lucasabrisolla@gmail.com>
2024-03-11 20:03:09 -0300

    Arrays finished

--- maratona-java/src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais02.java
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais02.java
@@ -3,7 +3,12 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula08ArraysMultidimensionais02 {
     public static void main(String[] args) {
         int[][] arrayInt = new int[3][];
-        for(int[] arrayBase: arrayInt){
+        arrayInt[0] = new int[]{1,2,3};
+        arrayInt[1] = new int[]{1,2};
+        arrayInt[2] = new int[]{1,2,3,4,5};
+
+        int[][] arrayInt2 = {{1,2,3},{1,8,9},{1,9,9,9,9}};
+        for(int[] arrayBase: arrayInt2){
             for(int num:arrayBase){
                 System.out.println(num);
             }
