6dd3cd8fc193c22a6527136057d3c68291c1f289
lucas-brisolla <lucasabrisolla@gmail.com>
2024-06-17 11:21:32 -0300

    setters, getters and constructors added

--- SantanderCoders/src/Animais/Cachorro.java
+++ SantanderCoders/src/Animais/Cachorro.java
@@ -1,27 +1,93 @@
 package Animais;
 
 public class Cachorro {
-    public String nome;
-    public String cor;
-    public int idade;
-    public double peso;
-    public char sexo;
-    public String reacao;
+    private String nome = "Cerberus";
+    private String cor = "Preto";
+    private int idade = 9;
+    private double peso = 5.32;
+    private char sexo = 'M';
+    private String reacao;
+
+    public Cachorro(){}
+    public Cachorro(String nome, String cor, int idade, double peso, char sexo, String reacao) {
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+        this.peso = peso;
+        this.sexo = sexo;
+        this.reacao = reacao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getReacao() {
+        return reacao;
+    }
+
+    public void setReacao(String reacao) {
+        this.reacao = reacao;
+    }
+}
 
     public void latir() {
         System.out.println("AU AU");
     }
 
     public String interagir(String acao) {
-        if (acao.equals("carinho")) {
-            this.reacao = "feliz";
-        } else if (acao.equals("pisar no rabo")) {
-            latir();
-            System.out.println("Voce pisou no rabo do cachorro!");
-            this.reacao = "triste";
-        } else {
-            this.reacao = "neutro";
+//        if (acao.equals("carinho")) {
+//            this.reacao = "feliz";
+//        } else if (acao.equals("pisar no rabo")) {
+//            latir();
+//            System.out.println("Voce pisou no rabo do cachorro!");
+//            this.reacao = "triste";
+//        } else {
+//            this.reacao = "neutro";
+//        }
+
+        switch (acao){
+            case "carinho": getReacao() = "feliz"; break;
+            case "pisar no rabo": this.reacao = "triste"; break;
+            case "dar comida": this.reacao = "satisfeito"; break;
+            default: this.reacao = "neutro"; break;
         }
-        return reacao;
+        return this.reacao;
     }
 }
--- SantanderCoders/src/Main.java
+++ SantanderCoders/src/Main.java
@@ -3,13 +3,15 @@ import Animais.Cachorro;
 public class Main {
     public static void main(String[] args) {
         Cachorro doginho = new Cachorro();
-        doginho.nome = "Cerberus";
-        doginho.cor = "Preto";
-        doginho.idade = 9;
-        doginho.sexo = 'M';
+        Cachorro doguinho2 = new Cachorro("Cerberus","Preto")
+//        doginho.nome = "Cerberus";
+//        doginho.cor = "Preto";
+//        doginho.idade = 9;
+//        doginho.sexo = 'M';
 
         System.out.println(doginho.interagir("O cachorro está "));
         System.out.println(doginho.interagir("pisar no rabo"));
+        System.out.println(doginho.interagir("dar comida"));
         System.out.println("O cachorro está " + doginho.interagir("carinho"));
     }
 }
